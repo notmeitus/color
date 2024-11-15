1487d543b2eef22ea0f9f975f3c6645c5bdd2297
shiva! <notlinux3@gmail.com>
2024-11-15 19:34:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 let button = document.querySelector(".btn");
+let color;
 button.addEventListener("click",function(){
    let head = document.querySelector("h1");
    let c =fun();
    head.innerText = c;
   
    let back = document.querySelector(".box");
-  back.style.backgroundColor =c;
+  back.style.backgroundColor = c;
 });
 
 function fun(){
     let r = Math.floor(Math.random()*255);
     let g = Math.floor(Math.random()*255);
     let b = Math.floor(Math.random()*255);
-    let color = `rgb(${r},${g},${b})`;
+     color = `rgb(${r},${g},${b})`;
     return color;
    
 };
+let btn = document.querySelector(".copy");
+btn.addEventListener("click",function(){
+    let copi = color;
+});
